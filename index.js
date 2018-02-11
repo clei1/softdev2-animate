09bb4a597006df0be2af462069ca11cc9e0ba1c0
clei1 <clei1@stuy.edu>
2018-02-11 18:23:45 -0500

    work01

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var c, ctx, clr, ps;
+var c, ctx, clr, ps, cir, box;
 
 c = document.getElementById("slate");
 ctx = c.getContext("2d");
 clr = document.getElementById("clear");
 ps = document.getElementById("pause");
+cir = document.getElementById("cir");
+box = document.getElementById("box");
 
 var requestID;
 
@@ -29,6 +31,33 @@ var start = function(){
     circle();
 };
 
+var dvd = function(){
+    
+    window.cancelAnimationFrame(requestID);
+    
+    var x = 0;
+    var y = 0;
+
+    var dx = 4.2;
+    var dy = 3.4;
+        
+    var dvd = function(){
+	x += dx;
+	y += dy;
+	if (x < 0 ||  x + 10 > c.width){
+	    dx *= -1;
+	}
+	if( y < 0 || y + 20 > c.height){
+	    dy *= -1;
+	}
+	ctx.clearRect(0, 0, c.width, c.height);
+	ctx.beginPath();
+	ctx.fillRect(x, y, 10, 20); 
+	requestID = window.requestAnimationFrame(dvd);
+    };
+    dvd();
+};
+
 var pause = function(){
     window.cancelAnimationFrame(requestID);
 };
@@ -38,6 +67,8 @@ var clear = function(){
     ctx.clearRect(0, 0, 500, 500);
 }
 
-c.addEventListener("click", start);
 clr.addEventListener("click", clear);
 ps.addEventListener("click", pause);
+
+cir.addEventListener("click", start);
+box.addEventListener("click", dvd);
